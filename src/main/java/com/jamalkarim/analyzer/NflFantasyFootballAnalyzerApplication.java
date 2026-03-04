ff90c46969114de2e13db95ddf06b21a7d04e862
Jamal-Karim <1jamalkarim@gmail.com>
2026-03-04 15:52:42 -0800

    Initial commit: Spring Boot NFL Analyzer Setup

---
+++ src/main/java/com/jamalkarim/analyzer/NflFantasyFootballAnalyzerApplication.java
@@ -0,0 +1,13 @@
+package com.jamalkarim.analyzer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NflFantasyFootballAnalyzerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NflFantasyFootballAnalyzerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jamalkarim/analyzer/NflFantasyFootballAnalyzerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jamalkarim.analyzer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NflFantasyFootballAnalyzerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
