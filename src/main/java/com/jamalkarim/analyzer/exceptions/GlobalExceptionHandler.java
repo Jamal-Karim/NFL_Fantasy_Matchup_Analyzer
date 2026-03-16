9a56822306d1aa53296834540960c316918550bd
Jamal-Karim <1jamalkarim@gmail.com>
2026-03-15 17:46:25 -0700

    Add custom error handling for fetching a player

---
+++ src/main/java/com/jamalkarim/analyzer/exceptions/GlobalExceptionHandler.java
@@ -0,0 +1,19 @@
+package com.jamalkarim.analyzer.exceptions;
+
+
+import com.jamalkarim.analyzer.dto.response.ApiResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ApiResponse<Void>> handleAllExceptions(Exception ex) {
+        ApiResponse<Void> response = ApiResponse.error("An unexpected error occurred: " + ex.getMessage());
+
+        return new ResponseEntity<>(response, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+}
